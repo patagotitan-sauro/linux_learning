9f0879947f5842d22fb95cb7d64bc61a75f260cb
radino <radino.comercial@gmail.com>
2026-05-13 18:14:40 -0300

    braveRemove.sh and historyCommand.sh

---
+++ historyCommand.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# (1) history basic command
+history
+
+# (2) !nro (bang command with the number)
+
+# (3) sudo !nro (to make a sudo command with a history)
+
+# (4) include the date and time to the history command in .bashrc
+# Set history format to include timestamps
+# HISTTIMEFORMAT="%Y-%m-%d %T " (include this line in .bashrc)
+
+# to ignore a space in the history command
+# HISTCONTROL=ignoreboth
+
+# (5) history nro (to get the last nro command)
+
+# history | grep <padrao_de_procura>
+
